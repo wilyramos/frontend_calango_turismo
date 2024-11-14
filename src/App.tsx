0f4e61f4cd44184e72ceff0a31484c2783415f84
wily ramos <wily.ramos1@unmsm.edu.pe>
2024-11-13 21:46:47 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,35 @@ import Recomendations from "./pages/Recomendations"
 export default function App() {
   return (
     <div className="App">
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<div>Not Found</div>} />
-        <Route path="/login" element={<Login />} />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<div>Not Found</div>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registrar" element={<div>Register</div>} />
+          <Route path="/olvide-password" element={<div>Olivde passowrd</div>} />
+          <Route path="/confirmar/:id" element={<div>Confirm</div>} />
 
-        <Route path="/categories" element={<div>Categories</div>} />
-        <Route path="/saved" element={<Recomendations />} />
 
-      </Routes>
-      <FooterSection />
-    </Router>
+          <Route path="/categories" element={<div>Categories</div>} />
+          <Route path="/saved" element={<Recomendations />} />
+
+
+          <Route path="/admin" element={<div>Admin Dashboard</div>}>
+            <Route path="dashboard" element={<div>Dashboard</div>} />
+          </Route>
+        
+        
+        
+        </Routes>
+
+        <FooterSection />
+      </Router>
+
+      
+
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,9 @@ export default function Navbar() {
     <nav className="bg-blue-600 text-white shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <h1 className="text-xl font-bold">Tourism Calango</h1>
+          <a href="/" className="text-2xl font-extrabold px-4">
+            Tourism Calango
+          </a>
 
           <div className="hidden md:flex items-center space-x-4">
             <form onSubmit={handleSearch} className="flex space-x-2">
